f80c9bdc3acfdb7682616f1b90018a2775f892b9
spikying <spiky.ing@gmail.com>
2020-09-02 11:17:53 -0400

    build:command doesn't save automatically now

--- src/commands/build/compact.ts
+++ src/commands/build/compact.ts
@@ -1,4 +1,5 @@
 import { d } from '../base'
+import { flags } from '@oclif/command'
 import Command from '../initialized-base'
 
 const debug = d('build:compact')
@@ -7,15 +8,24 @@ export class Compact extends Command {
   static description = `Only compacts numbers of files`
 
   static flags = {
-    ...Command.flags
+    ...Command.flags,
+    save: flags.boolean({
+      char: 's',
+      description: 'Commit to git at the same time.',
+      default: false
+    })
   }
 
   static hidden = false
 
   async run() {
     debug('Running Build:compact command')
+    const { args, flags } = this.parse(Compact)
+
     await this.coreUtils.preProcessAndCommitFiles('Before compacting file numbers')
     await this.coreUtils.compactFileNumbers()
-    await this.coreUtils.preProcessAndCommitFiles('Compacted file numbers')
+    if (flags.save) {
+      await this.coreUtils.preProcessAndCommitFiles('Compacted file numbers')
+    }
   }
 }
